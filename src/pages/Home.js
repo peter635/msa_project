54705d73c218f97576cb1f8ebbd6e7f68e03caae
peter ma <peter635@gmail.com>
2024-10-29 16:35:03 +0900

    change home page back

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,12 @@
-// src/Home.js
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
-function Home() {
-  return (
-    <div>
-        <title>Your Home Page Title1</title>
-        {/* 其他头部标签可以在这里添加 */}
-      <h1>Welcome to the Home Page1</h1>
-      {/* 主页内容 */}
-    </div>
-  );
-}
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>Welcome to the Home Page!</p>
+        </div>
+    );
+};
 
 export default Home;
-
