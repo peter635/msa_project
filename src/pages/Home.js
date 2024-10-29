3c00e22e7c42d3de2830bc942ace9dd5c23cf9f4
peter ma <peter635@gmail.com>
2024-10-29 16:32:37 +0900

    adding verify tag into index.html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,8 @@ import { Helmet } from 'react-helmet';
 function Home() {
   return (
     <div>
-      <Helmet>
-        <meta name="msvalidate.01" content="C9FD1D91330EE896AC7D11D096292318" />
         <title>Your Home Page Title1</title>
         {/* 其他头部标签可以在这里添加 */}
-      </Helmet>
       <h1>Welcome to the Home Page1</h1>
       {/* 主页内容 */}
     </div>
